832819d52cd8518b8888e274c1b198f82d80141e
jlee927 <jlee927@uic.edu>
2025-02-05 11:32:15 -0600

    added automatic scroll to top

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -3,11 +3,13 @@ import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Projects from './pages/Projects'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from './components/ScrollToTop';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <main>
+        <ScrollToTop />
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
