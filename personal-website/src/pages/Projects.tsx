2f7f08375eada10e0edbe4db505b1a37ae318f0f
jlee927 <jlee927@uic.edu>
2025-03-12 11:47:53 -0500

    added interview diver

--- personal-website/src/pages/Projects.tsx
+++ personal-website/src/pages/Projects.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion"
 import styles from '../styles/Projects.module.css'
 import fetch from '../assets/fetch.png'
 import poker from '../assets/3CardPoker.png'
+import interview from '../assets/interview_diver.png'
 
 const Projects: React.FC = () => {
   return (
@@ -16,6 +17,24 @@ const Projects: React.FC = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
         >
+
+          <div className={styles.project_container}>
+            <div className={styles.project_description}>
+              <h1 className={styles.title}>Interview Diver</h1>
+              <br />
+              <p style={{ color: "pink" }}>
+                Interview Diver is an AI-powered interview practice platform that helps users improve their interviewing skills through
+                real-time voice input and AI-generated feedback. The projects frontend was created using React TypeScript frontend, while the
+                backend was created with Flask and SQL. Postgres stores user data and processes responses using OpenAI. Users can customize their practice sessions by selecting profession, difficulty,
+                number of questions, and experience level
+              </p>
+            </div>
+
+            <div className={styles.project_preview}>
+              <img src={interview} className={styles.project_image} />
+            </div>
+          </div>
+
 
           <div className={styles.project_container}>
             <div className={styles.project_description}>
